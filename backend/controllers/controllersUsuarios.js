eb7ac259fb3772047c2284ff6c20a658f35eee5c
Giuliani XD <Giuliani_Hoil@Giuliani-Hoil.maltas.com>
2025-02-27 15:16:56 -0600

    video 12-13.1-13.2

--- backend/controllers/controllersUsuarios.js
+++ backend/controllers/controllersUsuarios.js
@@ -1,9 +1,10 @@
 function guardarUsuario(usuario) {
 
 try {
-    const {id, nombreCompleto, correo, contraseña}=usuario;
+    //const {id, nombreCompleto, correo, contraseña}=usuario;
     const sheetUsuarios=obtenerSheet(env_().SH_REGISTRO_USUARIOS);
-    sheetUsuarios.appendRow([id, nombreCompleto, correo, contraseña]); 
+    Insert(JSON.parse(usuario), sheetUsuarios);
+    //sheetUsuarios.appendRow([id, nombreCompleto, correo, contraseña]); 
     return {
         titulo:"Registro exitoso",
         descripcion: "Ya se encuentra el usuario en la base de datos.",
@@ -19,6 +20,7 @@ try {
 
 }
 
-function listarUsuarios() {
-    return obtenerDatos(env_().SH_REGISTRO_USUARIOS);
+function listarUsuarios(id = undefined) {
+    //return obtenerDatos(env_().SH_REGISTRO_USUARIOS);
+    return JSON.stringify(_read(obtenerSheet(env_().SH_REGISTRO_USUARIOS), id));
 }
